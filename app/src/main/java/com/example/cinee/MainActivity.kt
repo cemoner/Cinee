f32332ccdcc38a18d575c76411cc95a50655896b
cemoner <cemoner@outlook.com>
2025-03-28 03:39:54 +0300

    theme setup(color scheme,typography)

--- app/src/main/java/com/example/cinee/MainActivity.kt
+++ app/src/main/java/com/example/cinee/MainActivity.kt
@@ -1,16 +1,32 @@
 package com.example.cinee
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonColors
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Devices
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.tooling.preview.PreviewDynamicColors
+import androidx.compose.ui.tooling.preview.PreviewLightDark
+import androidx.compose.ui.tooling.preview.PreviewScreenSizes
+import androidx.compose.ui.unit.dp
+import androidx.sqlite.db.SupportSQLiteOpenHelper
 import com.example.cinee.ui.theme.CineeTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +36,9 @@ class MainActivity : ComponentActivity() {
         setContent {
             CineeTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Surface {
+                        AppScreen(modifier = Modifier.padding(innerPadding))
+                    }
                 }
             }
         }
@@ -31,17 +46,62 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun AppScreen(modifier: Modifier = Modifier) {
+    Scaffold(
+        modifier = modifier.background(MaterialTheme.colorScheme.background))
+    { innerPadding ->
+        Column(
+            modifier = Modifier
+                .padding(innerPadding)
+                .fillMaxSize(),
+            verticalArrangement = Arrangement.SpaceBetween,
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.primary)
+            ) {
+                Text(text = "Hello World!", style = MaterialTheme.typography.headlineLarge, color = MaterialTheme.colorScheme.onPrimary)
+            }
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.secondary)
+            ) {
+                Text(text = "Hello World!", style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.onSecondary)
+            }
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.tertiary)
+            ) {
+                Text(text = "Hello World!", style = MaterialTheme.typography.headlineSmall, color = MaterialTheme.colorScheme.onTertiary)
+            }
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.error)
+            ) {
+                Text(text = "Hello World!", style = MaterialTheme.typography.headlineLarge, color = MaterialTheme.colorScheme.onError)
+            }
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.surface)
+            ) {
+                Text(text = "Hello World!", style = MaterialTheme.typography.headlineMedium, color = MaterialTheme.colorScheme.onSurface)
+            }
+            Button(onClick = { /*TODO*/ },
+                colors = ButtonDefaults.buttonColors(containerColor = MaterialTheme.colorScheme.background)
+            ) {
+                Text(text = "Hello World!", style = MaterialTheme.typography.headlineSmall, color = MaterialTheme.colorScheme.onBackground)
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+@Preview(
+    showBackground = true,
+    showSystemUi = true,
+    uiMode = Configuration.UI_MODE_NIGHT_NO,
+    device = Devices.NEXUS_5
+)
+@PreviewLightDark
+@PreviewDynamicColors
 @Composable
-fun GreetingPreview() {
+fun AppScreenPreview() {
     CineeTheme {
-        Greeting("Android")
+        AppScreen(modifier = Modifier.padding(16.dp))
     }
 }
