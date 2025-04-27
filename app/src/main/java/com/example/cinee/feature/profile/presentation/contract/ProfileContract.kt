6f6247a25fff0a46281c9ed2b0257e1e5defa626
cemoner <cemoner@outlook.com>
2025-04-27 15:19:03 +0300

    create profile contract for mvi implementation and initiate profile viewmodel mvi setup

---
+++ app/src/main/java/com/example/cinee/feature/profile/presentation/contract/ProfileContract.kt
@@ -0,0 +1,35 @@
+package com.example.cinee.feature.profile.presentation.contract
+
+interface ProfileContract {
+    sealed interface UiState {
+        object Loading : UiState
+        data class Success(
+            val userName: String,
+            val email: String,
+            val phoneNumber: String,
+            val profilePictureUrl: String,
+            val displayState:DisplayState = DisplayState.View
+        ) : UiState
+        data class Error(val message : String) : UiState
+    }
+
+    sealed interface UiAction {
+        object SignOut : UiAction
+        object ChangeDisplayState : UiAction
+    }
+
+    sealed interface SideEffect {
+        object NavigateToSignInScreen : SideEffect
+        data class ShowToast(val message: String) : SideEffect
+    }
+}
+
+sealed interface DisplayState {
+    data class Edit(
+        val userName: String,
+        val email: String,
+        val phoneNumber: String,
+        val profilePictureUrl: String
+    ) : DisplayState
+    object View : DisplayState
+}
--- app/src/main/java/com/example/cinee/feature/profile/presentation/viewmodel/ProfileViewModel.kt
+++ app/src/main/java/com/example/cinee/feature/profile/presentation/viewmodel/ProfileViewModel.kt
@@ -1,19 +1,19 @@
 package com.example.cinee.feature.profile.presentation.viewmodel
 
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.example.cinee.feature.auth.domain.usecase.SignOutUseCase
+import com.example.cinee.feature.profile.presentation.contract.ProfileContract.UiState
+import com.example.cinee.feature.profile.presentation.contract.ProfileContract.UiAction
+import com.example.cinee.feature.profile.presentation.contract.ProfileContract.SideEffect
+import com.example.cinee.mvi.MVI
+import com.example.cinee.mvi.mvi
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class ProfileViewModel @Inject constructor(
-    private val signOutUseCase: SignOutUseCase,
+class ProfileViewModel @Inject constructor(private val signOutUseCase: SignOutUseCase)
+    : ViewModel(), MVI<UiState, UiAction, SideEffect> by mvi(initialUiState()) {
 
-):ViewModel() {
-
-    fun signOut() = viewModelScope.launch{
-        signOutUseCase()
-    }
 }
+
+private fun initialUiState():UiState = UiState.Loading
